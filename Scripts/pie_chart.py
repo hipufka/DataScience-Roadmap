1ca527a60f2e44f61b9e1c7f78af951b682875b3
zubareva <anna.zubareva@appflame.com>
2023-05-22 10:18:34 +0300

    additional viz & code

---
+++ Scripts/pie_chart.py
@@ -0,0 +1,18 @@
+# Declare a figure with a custom size
+fig = plt.figure(figsize=(5, 5))
+
+# labels for the two classes
+labels = 'Positives', 'Negative'
+
+# Sizes for each slide
+sizes = [len(all_positive_tweets), len(all_negative_tweets)] 
+
+# Declare pie chart, where the slices will be ordered and plotted counter-clockwise:
+plt.pie(sizes, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+
+# Equal aspect ratio ensures that pie is drawn as a circle.
+plt.axis('equal')  
+
+# Display the chart
+plt.show()
